ad9738bf8493c9acecef7c4fc5fbb0a033c6f51a
Макаров Степан Александрович <st.mkv03@gmail.com>
2024-11-30 13:40:46 +0300

    feat(get_questions_and_answer): Скрипт для парсинка файла и разбиения на ответы-вопросы

---
+++ get_questions.py
@@ -0,0 +1,33 @@
+import os
+import re
+from zipfile import ZipFile
+
+
+def extract_zip(zip, extract_dir):
+    if not os.path.isdir("quiz"):
+        with ZipFile("quiz-questions.zip", "r") as zipf:
+            zipf.extractall("quiz")
+
+
+def get_answer_and_questions(filename, code):
+    with open("1vs1200.txt", "r", encoding="KOI8-R") as file:
+        questions_and_answers = file.read()
+
+        question_pattern = r"(Вопрос\s\d+:.*?)(?=Ответ:)"
+        answer_pattern = r"(Ответ:.*?)(?=\n\n|$)"
+
+        questions = re.findall(
+            question_pattern, questions_and_answers, re.DOTALL
+        )
+        answers = re.findall(answer_pattern, questions_and_answers, re.DOTALL)
+
+        questions = [q.replace("\n", " ").strip() for q in questions]
+        answers = [a.replace("\n", " ").strip() for a in answers]
+    return questions, answers
+
+
+if __name__ == "__main__":
+    extract_zip("quiz-questions.zip", "quiz")
+    questions, answers = get_answer_and_questions("1vs1200.txt", "KOI8-R")
+    q = dict(zip(questions, answers))
+    print(q)
